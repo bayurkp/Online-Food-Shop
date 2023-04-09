082566e81c8325ed0864823253a405a36258d307
Bay <bayurkp194@gmail.com>
2023-04-10 06:34:20 +0800

    Membuat method intro()

--- src/App.java
+++ src/App.java
@@ -1,29 +1,41 @@
 // import java.util.*;
 
 public class App {
+    public static final String NORMAL = "\033[0;0m";
+    public static final String BOLD = "\033[0;1m";
+
+    public static void clearScreen() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
+
+    public static void intro() {
+        clearScreen();
+        System.out.println(BOLD + "\nFast-food" + NORMAL);
+        System.out.println("---------------");
+        System.out.println("Pilihan terbaik untuk keluarga");
+        System.out.print("\nTekan enter untuk melanjutkan....");
+        Input.getString();
+    }
+
     public static void login() {
+        clearScreen();
+        System.out.println(BOLD + "LOGIN" + NORMAL);
         System.out.print("Username\n> ");
         String username = Input.getString();
         System.out.print("Password\n> ");
         String password = Input.getPassword();
 
-        System.out.println(password);
         if (username.equals(Admin.getAdminUsername()) && password.equals(Admin.getAdminPass())) {
             Admin.menu();
         } else if (username.equals(Customer.getCustUsername()) && password.equals(Customer.getCustPass())) {
             Customer.menu();
         } else {
-            System.out.print("\033[H\033[2J");
-            System.out.flush();
-
             login();
         }
     }
 
     public static void main(String[] args) {
-        // login();
-        Data.addRestaurant();
-        System.out.println(Data.getRestaurants().get(0).getFoods());
-        // restaurants.get(0).getDrinks();
+        intro();
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        // System.out.print("\033[H\033[2J");
-        // System.out.flush();
-
-        // // App.login();
-
-        // int hehe = Input.getInteger();
-        // System.out.println("num: " + hehe);
+        App.intro();
+        App.login();
     }
 }
