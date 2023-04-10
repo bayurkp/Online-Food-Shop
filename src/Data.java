f8f432ec339c2fe4a819a54edb8acb88e6987ed9
Bay <bayurkp194@gmail.com>
2023-04-11 07:16:05 +0800

    Memperbaiki error pada method showRestaurant()

--- src/Data.java
+++ src/Data.java
@@ -74,6 +74,7 @@ public class Data {
         System.out.println(App.BOLD + "\nTampilkan Restaurant" + App.NORMAL);
         System.out.println("---------------");
 
+        // Jika belum ada restaruant yang ditambahkan maka akan tampil teks berikut
         if (restaurants.size() == 0) {
             System.out.println("Tidak ada restaurant teregistrasi.");
             System.out.print("*(Tekan enter untuk melanjutkan....)");
@@ -81,15 +82,14 @@ public class Data {
             return;
         }
 
-        System.out.println(restaurants.get(0));
-        System.out.println(restaurants.get(1));
-
+        // Melakukan looping untuk menampilkan daftar restaurant yang ditambahkan
         for (int i = 0; i < restaurants.size(); i++) {
             String restaurantName = restaurants.get(i).getName();
             String restaurantAddress = restaurants.get(i).getAddress();
             System.out.printf("%d. %s-%s\n", i + 1, restaurantName, restaurantAddress);
         }
 
+        // Menerima input untuk memilih detail menu dari restaurant dengan ID yang diinput
         System.out.println("Masukkan ID restaurant untuk detail menu");
         System.out.print("*(masukkan 0 untuk kembali)\n> ");
         int idRestaurant = 0;
@@ -102,34 +102,39 @@ public class Data {
             }
         } while (idRestaurant < 0 && idRestaurant > restaurants.size());
 
+        // Penyimpanan sementara untuk menu makanan dan minuman
         ArrayList<ArrayList<String>> foods = new ArrayList<>();
         ArrayList<ArrayList<String>> drinks = new ArrayList<>();
-
         foods = restaurants.get(idRestaurant - 1).getFoods();
         drinks = restaurants.get(idRestaurant - 1).getDrinks();
+
+        // Menampilkan daftar menu makanan dari restaurant yang dipilih
+        System.out.printf(App.BOLD + "\n%s-%s\n" + App.NORMAL, restaurants.get(idRestaurant - 1).getName(), restaurants.get(idRestaurant - 1).getAddress());
+        System.out.println("---------------");
         System.out.println(App.BOLD + "\nMenu Makanan" + App.NORMAL);
         System.out.println("---------------");
-        for (int i = 0; i < foods.size(); i++) {
-            System.out.printf("%d. %s\t\t%s\n", i + 1, foods.get(i), foods.get(i).get(0));
+        if (foods.size() == 0) {
+            System.out.println("Tidak ada makanan.");
+        } else {
+            for (int i = 0; i < foods.size(); i++) {
+                System.out.printf("%d. %s\t\tRp.%s\n", i + 1, foods.get(i).get(0), foods.get(i).get(1));
+            }
         }
 
+        // Menampilkan daftar menu minuman dari restaurant yang dipilih
         System.out.println(App.BOLD + "\nMenu Minuman" + App.NORMAL);
         System.out.println("---------------");
-        for (int i = 0; i < drinks.size(); i++) {
-            System.out.printf("%d. %s\t\t%s\n", i + 1, drinks.get(i), drinks.get(i).get(0));
+        if (drinks.size() == 0) {
+            System.out.println("Tidak ada minuman.");
+        } else {
+            for (int i = 0; i < drinks.size(); i++) {
+                System.out.printf("%d. %s\t\tRp.%s\n", i + 1, drinks.get(i).get(0), drinks.get(i).get(1));
+            }        
         }
         Input.getString();
     }
 
     public static ArrayList<Restaurant> getRestaurants() {
         return restaurants;
-    }
-
-    public static void main(String[] args) {
-        Restaurant tes1 = new Restaurant("hehe", "hehe");
-        Restaurant tes2 = new Restaurant("xixi", "xoxo");
-        restaurants.add(tes1);
-        restaurants.add(tes2);
-        showRestaurant();
     }
 }
