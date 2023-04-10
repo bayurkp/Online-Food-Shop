4a3954e385e8d00e9dd08d58a6b3b3b1ef78a3a8
Bay <bayurkp194@gmail.com>
2023-04-11 07:34:13 +0800

    Memindahkan method addRestaurant() ke class Admin

--- src/Admin.java
+++ src/Admin.java
@@ -34,7 +34,7 @@ public class Admin {
                 Data.showRestaurant();
                 break;
             case 2:
-                Data.addRestaurant();
+                addRestaurant();
                 break;
             case 3:
                 // deleteRestaurant();
@@ -47,6 +47,68 @@ public class Admin {
         }
 
         menu();
+    }
+
+        // Method untuk menambahkan data restaurant
+    public static void addRestaurant() {
+        App.clearScreen();
+        System.out.println(App.BOLD + "\nTambah Restaurant" + App.NORMAL);
+        System.out.println("---------------");
+
+        System.out.print("Masukkan nama restaurant\n> ");
+        String restaurantName = Input.getString();
+        restaurantName = App.capitalizeWord(restaurantName);
+
+        System.out.print("Masukkan alamat restaurant\n> ");
+        String restaurantAddress = Input.getString();
+        restaurantAddress = App.capitalizeWord(restaurantAddress);
+        App.clearScreen();
+
+        Restaurant restaurant = new Restaurant(restaurantName, restaurantAddress);
+        int select = 0;
+        int confirm = 0;
+        do {
+            App.clearScreen();
+            System.out.printf(App.BOLD + "Restaurant %s - %s\n" + App.NORMAL, restaurantName, restaurantAddress);
+            System.out.println("---------------");
+            System.out.println("1. Makanan");
+            System.out.println("2. Minuman");
+            System.out.print("*(Mohon masukkan kode angka untuk melanjutkan)\n> ");
+            do {
+                select = Input.getInteger();
+                if (select != 1 && select != 2) {
+                    System.out.print("Input invalid. Mohon ulangi\n> ");
+                }
+            } while (select != 1 && select != 2);
+            
+            System.out.print("Masukkan nama menu\n> ");
+            String menuName = Input.getString();
+            menuName = App.capitalizeWord(menuName);
+
+            System.out.print("Masukkan harga menu (Rp)\n> ");
+            String menuPrice = Double.toString(Input.getDouble());
+
+            switch (select) {
+                case 1:
+                    restaurant.addFood(menuName, menuPrice);
+                    break;
+                case 2:
+                    restaurant.addDrinks(menuName, menuPrice);
+                    break;
+                default:
+                    System.out.print("Input invalid. Mohon ulangi\n> ");
+            }
+
+            System.out.print("Apakah Anda ingin menambah menu lainnya?\n*(1 untuk melanjutkan dan 0 untuk tidak)\n> ");
+            do {
+                confirm = Input.getInteger();
+                if (confirm != 1 && confirm != 0) {
+                    System.out.print("Input invalid. Mohon ulangi\n> ");
+                }
+            } while (confirm != 1 && confirm != 0);
+        } while (confirm != 0);
+        
+        Data.addRestaurants(restaurant);
     }
 
 }
--- src/Data.java
+++ src/Data.java
@@ -3,70 +3,6 @@ import java.util.*;
 public class Data {
     // Array untuk menyimpan data restaurant
     private static ArrayList<Restaurant> restaurants = new ArrayList<>();
-        
-    // Method untuk menambahkan data restaurant
-    public static void addRestaurant() {
-        App.clearScreen();
-        System.out.println(App.BOLD + "\nTambah Restaurant" + App.NORMAL);
-        System.out.println("---------------");
-
-        System.out.print("Masukkan nama restaurant\n> ");
-        String restaurantName = Input.getString();
-        restaurantName = App.capitalizeWord(restaurantName);
-
-        System.out.print("Masukkan alamat restaurant\n> ");
-        String restaurantAddress = Input.getString();
-        restaurantAddress = App.capitalizeWord(restaurantAddress);
-        App.clearScreen();
-
-        Restaurant restaurant = new Restaurant(restaurantName, restaurantAddress);
-        int select = 0;
-        int confirm = 0;
-        do {
-            App.clearScreen();
-            System.out.printf(App.BOLD + "Restaurant %s - %s\n" + App.NORMAL, restaurantName, restaurantAddress);
-            System.out.println("---------------");
-            System.out.println("1. Makanan");
-            System.out.println("2. Minuman");
-            System.out.print("*(Mohon masukkan kode angka untuk melanjutkan)\n> ");
-            do {
-                select = Input.getInteger();
-                if (select != 1 && select != 2) {
-                    System.out.print("Input invalid. Mohon ulangi\n> ");
-                }
-            } while (select != 1 && select != 2);
-            
-            System.out.print("Masukkan nama menu\n> ");
-            String menuName = Input.getString();
-            menuName = App.capitalizeWord(menuName);
-
-            System.out.print("Masukkan harga menu (Rp)\n> ");
-            String menuPrice = Double.toString(Input.getDouble());
-
-            switch (select) {
-                case 1:
-                    restaurant.addFood(menuName, menuPrice);
-                    break;
-                case 2:
-                    restaurant.addDrinks(menuName, menuPrice);
-                    break;
-                default:
-                    System.out.print("Input invalid. Mohon ulangi\n> ");
-            }
-
-            System.out.print("Apakah Anda ingin menambah menu lainnya?\n*(1 untuk melanjutkan dan 0 untuk tidak)\n> ");
-            do {
-                confirm = Input.getInteger();
-                System.out.println(confirm);
-                if (confirm == 1 && confirm == 0) {
-                    System.out.print("Input invalid. Mohon ulangi\n> ");
-                }
-            } while (confirm == 1 && confirm == 0);
-
-        } while (confirm != 0);
-        
-        restaurants.add(restaurant);
-    }
 
     // Method untuk menampilkan data restaruant
     public static void showRestaurant() {
@@ -97,10 +33,10 @@ public class Data {
             idRestaurant = Input.getInteger();
             if (idRestaurant == 0) {
                 return;
-            } else if (idRestaurant < 0 && idRestaurant > restaurants.size()) {
+            } else if (idRestaurant < 0 && idRestaurant >= restaurants.size()) {
                 System.out.print("Input invalid. Mohon ulangi\n> ");
             }
-        } while (idRestaurant < 0 && idRestaurant > restaurants.size());
+        } while (idRestaurant < 0 && idRestaurant >= restaurants.size());
 
         // Penyimpanan sementara untuk menu makanan dan minuman
         ArrayList<ArrayList<String>> foods = new ArrayList<>();
@@ -109,6 +45,7 @@ public class Data {
         drinks = restaurants.get(idRestaurant - 1).getDrinks();
 
         // Menampilkan daftar menu makanan dari restaurant yang dipilih
+        App.clearScreen();
         System.out.printf(App.BOLD + "\n%s-%s\n" + App.NORMAL, restaurants.get(idRestaurant - 1).getName(), restaurants.get(idRestaurant - 1).getAddress());
         System.out.println("---------------");
         System.out.println(App.BOLD + "\nMenu Makanan" + App.NORMAL);
@@ -117,7 +54,7 @@ public class Data {
             System.out.println("Tidak ada makanan.");
         } else {
             for (int i = 0; i < foods.size(); i++) {
-                System.out.printf("%d. %s\t\tRp.%s\n", i + 1, foods.get(i).get(0), foods.get(i).get(1));
+                System.out.printf("%d. %s\t\tRp. %s\n", i + 1, foods.get(i).get(0), foods.get(i).get(1));
             }
         }
 
@@ -128,7 +65,7 @@ public class Data {
             System.out.println("Tidak ada minuman.");
         } else {
             for (int i = 0; i < drinks.size(); i++) {
-                System.out.printf("%d. %s\t\tRp.%s\n", i + 1, drinks.get(i).get(0), drinks.get(i).get(1));
+                System.out.printf("%d. %s\t\tRp. %s\n", i + 1, drinks.get(i).get(0), drinks.get(i).get(1));
             }        
         }
         Input.getString();
@@ -136,5 +73,9 @@ public class Data {
 
     public static ArrayList<Restaurant> getRestaurants() {
         return restaurants;
+    }
+
+    public static void addRestaurants(Restaurant restaurant) {
+        restaurants.add(restaurant);
     }
 }
