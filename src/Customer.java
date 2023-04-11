b79952cc92b04b20ba8b5a3c02e09654dfa34604
Bay <bayurkp194@gmail.com>
2023-04-12 05:07:17 +0800

    Membuat validasi untuk idMenu

--- src/Customer.java
+++ src/Customer.java
@@ -45,6 +45,8 @@ public class Customer {
             return;
         }
 
+        Restaurant restaurant = new Restaurant(Data.getRestaurants().get(idRestaurant - 1).getName(), Data.getRestaurants().get(idRestaurant - 1).getAddress());
+
         System.out.println(App.BOLD + "Tambahkan Pesanan" + App.NORMAL);
         System.out.println("---------------");
         System.out.println("Mohon masukkan ID menu.");
@@ -54,8 +56,25 @@ public class Customer {
         if (idMenu.equals("0")) {
             menu();
             return;
+        } else {
+            System.out.println(idMenuValidation(idMenu));
         }
-        System.out.println(Data.getRestaurants().get(idRestaurant - 1).getName());
+    }
+
+    public static boolean idMenuValidation(String idMenu) {
+        String[] parseIdMenu = idMenu.split("-");
+        if (parseIdMenu.length == 2) {
+            try {  
+                Integer.parseInt(parseIdMenu[0]);
+                if (parseIdMenu[1].equals("d") || parseIdMenu[1].equals("f") || parseIdMenu[1].equals("D") || parseIdMenu[1].equals("F")) {
+                    return true;
+                }
+            } catch(NumberFormatException e) {  
+                return false;
+            } 
+        } 
+        
+        return false;
     }
 
     public static void main(String[] args) {
